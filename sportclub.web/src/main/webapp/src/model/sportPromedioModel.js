378e9eaf664131c041876e45ec453f5c3dc7e358
TheBlackTux <diegorbaquero@gmail.com>
2014-08-18 11:56:53 -0500

    sportPromedio - Manejo de listas

---
+++ sportclub.web/src/main/webapp/src/model/sportPromedioModel.js
@@ -0,0 +1,23 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+define(['model/sportPromedioModel'], function() {
+//Aquí se define la estructura de un ítem de la lista. Note que el modelo extiende el modelo estándar backbone.
+    App.Model.SportPromedioModel = Backbone.Model.extend({
+        defaults: {
+         'name' : '',
+         'average' : ''
+        },
+        getDisplay: function(name) {
+         return this.get(name);
+        }
+        });
+//Aquí se define el modelo de la lista. El modelo de la lista extiende de Backbone.Collection. En el atributo ‘model’ se define el modelo  (definido arriba) que corresponde al molde de cada uno de los ítems de la lista.
+    App.Model.SportPromedioList = Backbone.Collection.extend({
+        model: App.Model.SportPromedioModel
+    });
+    return  App.Model.SportPromedioModel;
+});
